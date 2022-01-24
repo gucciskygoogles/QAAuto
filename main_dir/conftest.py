f46eaf8c8398b53ddcd26b3291ae8eb7f13f1721
Nick <iwillloveyou44@yandex.ru>
2022-01-24 18:37:03 +0300

    Parametrize added

--- main_dir/conftest.py
+++ main_dir/conftest.py
@@ -1,8 +1,20 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(scope="class")
-def browser():
-    browser = webdriver.Chrome()
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help='Выберите браузер, Chrome или Firefox')
+
+@pytest.fixture()
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome' or browser_name == 'Chrome':
+        browser = webdriver.Chrome()
+    elif browser_name == 'firefox' or browser_name == 'Firefox':
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError ('--browser_name доллжен быть Firefox или Chrome')
     yield browser
     browser.quit()
--- main_dir/test_selenium.py
+++ main_dir/test_selenium.py
@@ -4,12 +4,14 @@ from selenium.webdriver.common.by import By
 import pytest
 
 
-
+@pytest.mark.parametrize('transfer_protocol', ['http', 'https'])
 class TestAutoMvideo:
+
+    @pytest.mark.xfail
     @pytest.mark.find_something
-    def test_search_in_and_choose(self, browser):
+    def test_search_in_and_choose(self, browser, transfer_protocol):
         browser.implicitly_wait(3)
-        browser.get('https://www.mvideo.ru/')
+        browser.get(f'{transfer_protocol}://www.mvideo.ru/')
         search_bar = browser.find_element(By.ID, '1')
         search_bar.send_keys('iphone')
         search_bar.send_keys(Keys.RETURN)
@@ -18,9 +20,9 @@ class TestAutoMvideo:
         browser.find_element(By.CLASS_NAME, 'mv-main-button--content').click()
 
     @pytest.mark.with_assert
-    def test_find_store(self, browser):
+    def test_find_store(self, browser, transfer_protocol):
         browser.set_window_size(1936, 1056)
-        browser.get('https://www.mvideo.ru/')
+        browser.get(f'{transfer_protocol}://www.mvideo.ru/')
         browser.find_element(By.XPATH, '//a[contains(.,"Магазины")]').click()
         time.sleep(3)
         search_stores = browser.find_element(By.ID, 'frm-search-store')
@@ -32,20 +34,21 @@ class TestAutoMvideo:
         time.sleep(3)
 
     @pytest.mark.find_something
-    def test_premium_catalog(self, browser):
+    def test_premium_catalog(self, browser, transfer_protocol):
         browser.set_window_size(1936, 1056)
-        browser.get('https://www.mvideo.ru/')
+        browser.get(f'{transfer_protocol}://www.mvideo.ru/')
         browser.find_element(By.CLASS_NAME, 'catalog-button').click()
         time.sleep(3)
         browser.find_element(By.LINK_TEXT, 'Premium').click()
 
 
+@pytest.mark.parametrize('transfer_protocol', ['http', 'https'])
 class TestKinopoiskUI:
 
     @pytest.mark.with_assert
-    def test_find_film(self, browser):
+    def test_find_film(self, browser, transfer_protocol):
         browser.set_window_size(1936, 1056)
-        browser.get('https://www.kinopoisk.ru/')
+        browser.get(f'{transfer_protocol}://www.kinopoisk.ru/')
         search = browser.find_element(By.NAME, 'kp_query')
         search.click()
         search.send_keys('Она')
